18ce0cb9c4c7abd383f58024a5f973288b590d0a
JM Dev <juanma261005@gmail.com>
2024-12-04 18:59:45 -0300

    UI fixed

--- src/components/ProfileHeader.tsx
+++ src/components/ProfileHeader.tsx
@@ -4,7 +4,7 @@ import Social from "./Social";
 
 export default function ProfileHeader() {
   return (
-    <div className="overflow-x-hidden tracking-wide w-full flex flex-col h-auto py-8">
+    <div className="overflow-x-hidden tracking-wide w-full flex flex-col h-auto py-2">
       <div className="w-full h-auto flex py-2">
         <img
           className="ml-4 rounded-full w-28 h-28"
@@ -20,7 +20,7 @@ export default function ProfileHeader() {
       <p className="w-full mx-4 mt-4 max-md:text-sm flex-wrap text-xs">
         Disponibilidad: {myData.available}
       </p>
-      <div className="w-full mt-4 ml-3 flex flex-row max-md:text-sm">
+      <div className="w-full mt-4 ml-4 flex flex-row max-md:text-sm">
         <img
           className="max-md:w-5 max-md:h-5 w-5 h-5 invert"
           src={locationIcon}

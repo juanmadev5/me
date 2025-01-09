40033f9853f50beb862bc634043bcf1707dafab7
juanma261005 <juanma261005@gmail.com>
2025-01-09 13:27:51 +0000

    fix UI bugs

--- src/components/ProfileHeader.tsx
+++ src/components/ProfileHeader.tsx
@@ -4,7 +4,7 @@ import Social from "./Social";
 
 export default function ProfileHeader() {
   return (
-    <div className="overflow-x-hidden tracking-wide w-full flex flex-col h-auto py-2">
+    <div className="tracking-wide flex flex-col h-auto py-2">
       <div className="w-full h-auto flex py-2">
         <img
           className="ml-4 rounded-full w-28 h-28"
@@ -18,7 +18,7 @@ export default function ProfileHeader() {
         {myData.headerDescription}
       </p>
 
-      <div className="w-full mt-4 ml-4 flex flex-row max-md:text-sm">
+      <div className="mt-4 ml-4 flex flex-row max-md:text-sm">
         <img
           className="max-md:w-5 max-md:h-5 w-5 h-5 invert"
           src={locationIcon}
@@ -29,10 +29,10 @@ export default function ProfileHeader() {
         </p>
       </div>
       <Social />
-      <p className="w-full mx-4 mt-4 max-md:text-sm flex-wrap text-xs text-sky-500">
+      <p className="mx-4 mt-4 max-md:text-sm flex-wrap text-xs text-sky-500">
         Disponibilidad:
       </p>
-      <ul className="w-full mx-4 mt-4 max-md:text-sm text-xs flex flex-wrap gap-1 tracking-wider">
+      <ul className="mx-4 mt-4 max-md:text-sm text-xs flex flex-wrap gap-2">
         <li className="flex items-center border rounded-2xl border-sky-800">
           <span className="m-2">🕒 Hasta las 17:30 hs</span>
         </li>

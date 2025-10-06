a3d73afd4c9fc8a92e34ef8a189ddaeca82dc71e
JM Dev <juanma261005@gmail.com>
2025-10-05 21:56:45 -0300

    fix some margins

--- src/components/ProfileHeader.tsx
+++ src/components/ProfileHeader.tsx
@@ -25,13 +25,13 @@ export default function ProfileHeader({
 }: ProfileHeaderProps) {
   return (
     <div className="tracking-wide flex flex-col h-auto py-2">
-      <div className="w-full h-auto flex flex-col py-2 items-center gap-2">
+      <div className="w-full h-auto flex flex-col py-2 items-center gap-2 ml-2 mr-2">
         <img
           className="mt-8 border-4 border-primary rounded-full h-36"
           alt={devName}
           src={profilePhoto}
         />
-        <p className="mt-6 text-2xl text-primary">{fullName}</p>
+        <p className="mt-6 text-2xl text-primary text-center ml-2 mr-2">{fullName}</p>
         <p className="text-pretty text-center text-onSurface mt-4 max-md:text-sm">
           {headerDescription}
         </p>
